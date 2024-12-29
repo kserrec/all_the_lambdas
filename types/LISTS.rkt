d82fdbd7adeb489bce48887e4cf5c46b86bfc96e
Kyle Serrecchia <kserrec@gmail.com>
2024-12-29 13:04:01 -0800

    remade insert and replace and cleaned up - ready for writing tests of the list functions

--- types/LISTS.rkt
+++ types/LISTS.rkt
@@ -21,7 +21,7 @@
 
 ;===================================================
 
-; just testing things out here 
+; definitions for tests 
 
 (def l-0 = ((pair zero) nil))
 (def l-5 = ((pair five) nil))
@@ -50,53 +50,6 @@
 (def LIST-0-5-2-3 = ((_make-list nat) l-0-5-2-3))
 (def LIST-5-5-2-3 = ((_make-list nat) l-5-5-2-3))
 (def LIST-p4-n3-p4-p2 = ((_make-list int) l-p4-n3-p4-p2))
-
-
-; (displayln (read-any LIST-0-5-2-3))
-; (displayln (read-any LIST-5-5-2-3))
-; (displayln (read-any LIST-p4-n3-p4-p2))
-
-
-#|
-    De-Abstractify list of lists
-
-    list:list[
-        list:nat[0,5,2,3],
-        list:nat[5,5,2,3]
-    ]
-    ==
-    [
-        list:nat[0,5,2,3],
-        list:nat[5,5,2,3]
-    ]
-    ==
-    {
-        ; type - "_list" (four),
-        ; val  - {
-                    list:nat[0,5,2,3],
-                    list:nat[5,5,2,3]
-                 }
-    }
-    ==
-    {
-        ; type - "_list" (four),
-        ; val  - {
-                    {
-                        ; type - "_list" (four),
-                        ; val  - [nat:0,nat:5,nat:2,nat:3]
-                    },
-                    {
-                        {   
-                            ; type - "_list" (four),
-                            ; val  - [nat:0,nat:5,nat:2,nat:3]
-                        },
-                        NIl-list
-                    }
-    }
-
-
-|#
-
 
 ;===================================================
 
@@ -152,88 +105,24 @@
 
 (def FILTER G L = (((((((fully-type2 _filter-helper-typed) "FILTER") bool) ((make-obj bool) G)) _list) L) _list))
 
-;===================================================
-
-(define _fold-helper-typed (Y fold-helper-typed))
-
-(def fold-helper-typed f g i lst = 
-    (_if ((eq zero) (len lst))
-        _then i
-        _else ((lst (lambda (x)
-                    (lambda (y)
-                        (lambda (z)
-                            ((g x) (((f g) i) y))
-                        )
-                    )
-                ))  i)
-    )
-)
-
-(def FOLD G X L = (((((((((fully-type3 _fold) "FOLD") bool) ((make-obj bool) G)) (type X)) X) _list) (un-type-els L)) (type X)))
-
-
-
-(displayln (read-any (((FOLD add) FIVE) LIST-0-5-2-3)))
-
-(displayln (read-any (((FOLD mult) ONE) LIST-5-5-2-3)))
-
-(displayln (read-any (((FOLD multZ) posONE) LIST-p4-n3-p4-p2)))
-
-
-; (displayln (read-any LIST-p4-n3-p4-p2))
-
-(def LIST-OF-LIST = ((pair LIST-0-5-2-3) NIL-list))
-
-
-(def LIST-OF-LISTS = ((pair LIST-5-5-2-3) LIST-OF-LIST))
-(def LOL = ((make-obj _list) LIST-OF-LISTS))
-
-(displayln (read-any ((ind LIST-OF-LISTS) one)))
-
-; (displayln (read-any ((IND LOL) ONE)))
-
-; (displayln (read-any (((_fold app) LIST-OF-LISTS) NIL-list)))
-
-; (displayln "read NIL list")
-; (displayln (read-any NIL-list))
-
-; (displayln "read LIST OF LIST")
-; (displayln (read-any LIST-OF-LIST))
+; has to be passed the untyped version of g - haven't worked out a way to get it passed the typed version and have it work yet
+(def FOLD g X L = (((((((((fully-type3 _fold) "FOLD") bool) ((make-obj bool) g)) (type X)) X) _list) (un-type-els L)) (type X)))
 
 ;===================================================
 
 (def TAKE N L = (((((((fully-type2 _take) "TAKE") nat) N) _list) L) _list))
 
-(displayln (read-any ((TAKE TWO) LIST-5-5-2-3)))
-
-
 (def TAKE-TAIL N L = (((((((fully-type2 takeTail) "TAKE-TAIL") nat) N) _list) L) _list))
 
+(def INSERT X L I = 
+    ((APP
+        ((TAKE I) L))
+        ((pair _list) ((pair X) (val ((TAKE-TAIL ((SUB (LEN L)) I)) L))))))
 
-(displayln (read-any ((TAKE-TAIL TWO) LIST-5-5-2-3)))
-
-;===================================================
-
-; "fully-type" function returns falsely typed list by just tagging with a _list type, but it doesn't have typed elements so this extra wrapping with _make-list is needed
-(def INSERT X L I = ((_make-list (type X)) (val (((((((((fully-type3 insert) "INSERT") (type X)) X) _list) (un-type-els L)) nat) I) _list))))
-
-; (displayln "insert test")
-; (def inserted-LIST-5523 = (((INSERT FOUR) LIST-5-5-2-3) TWO))
-; (displayln (read-any inserted-LIST-5523))
-; (displayln (n-read (type inserted-LIST-5523)))
-; (displayln (n-read (type (val inserted-LIST-5523))))
-
-
-(def REPLACE X L I = ((_make-list (type X)) (val (((((((((fully-type3 replace) "REPLACE") (type X)) X) _list) (un-type-els L)) nat) I) _list))))
-
-(displayln "replace test")
-(def replaced-LIST-5523 = (((REPLACE FOUR) LIST-5-5-2-3) TWO))
-(displayln (read-any replaced-LIST-5523))
-; (displayln (n-read (type inserted-LIST-5523)))
-; (displayln (n-read (type (val replaced-LIST-5523))))
+(def REPLACE X L I = 
+    ((APP
+        ((TAKE I) L))
+        ((pair _list) ((pair X) (val ((TAKE-TAIL (PRED ((SUB (LEN L)) I))) L))))))
 
 
 (def DROP N L = (((((((fully-type2 _drop) "DROP") nat) N) _list) L) _list))
-
-(displayln "drop test")
-(displayln (read-any ((DROP ONE) LIST-5-5-2-3)))
