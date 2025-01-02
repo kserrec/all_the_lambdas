d5029b12198ba5400b107baf5cc6f96cda291616
Kyle Serrecchia <kserrec@gmail.com>
2025-01-02 07:58:30 -0800

    got recursive add function working for binary numbers of same length which don't have a final carry

--- binary-lists.rkt
+++ binary-lists.rkt
@@ -1,9 +1,9 @@
 #lang s-exp "macros/lazy-with-macros.rkt"
 (require "macros/macros.rkt")
 (provide (all-defined-out))
-(require "lists.rkt"
+(require "church.rkt"
+         "lists.rkt"
          "logic.rkt"
-         "church.rkt"
          "recursion.rkt")
 
 ;===================================================
@@ -33,11 +33,19 @@
     ~ FIRST FEW NUMBERS ~
 |#
 (def bin-zero = (_cons zero))
-(def bin-one = (_cons one))
-(def bin-two = (_cons one zero))
+; (def bin-one = (_cons one))
+; (def bin-two = (_cons one zero))
+(def bin-one = ((pair one) nil))
+(def bin-two = ((pair one) ((pair zero) nil)))
 (def bin-three = (_cons one one))
 (def bin-four = (_cons one zero zero))
 (def bin-five = (_cons one zero one))
+(def bin-ten = (_cons one zero one zero))
+(def bin-twenty = (_cons one zero one zero zero))
+(def bin-twenty-four = (_cons one one zero zero zero))
+
+(displayln ((l-read ((pair one) nil)) n-read))
+; (displayln ((l-read bin-one) n-read))
 
 ;===================================================
 
@@ -68,6 +76,8 @@
 ; (displayln (bin-read bin-three))
 ; (displayln (bin-read bin-four))
 ; (displayln (bin-read bin-five))
+
+
 
 (def bin-one-billion =
   (_cons one one one zero one one one zero 
@@ -109,14 +119,131 @@
 
 
 
+;===================================================
+
+; assumes only zeroes and ones exist
+(def isOne digit = (_not (isZero digit)))
+
+#|
+    The next two functions, 
+    get-place and get-new-carry, 
+    are based entirely off this truth table:
+    (zeroes are turned to false, ones to true)
+    current-carry | dig1 | dig2 | place-val | new-carry
+          0          0      0        0           0
+          0          1      0        1           0
+          0          0      1        1           0
+          0          1      1        0           1
+          1          0      0        1           0
+          1          1      0        0           1
+          1          0      1        0           1
+          1          1      1        1           1
+|#
+
+#|
+    ~ gets current place value ~
+        if ((carry) AND (not (xor dig1 dig2)))
+            -> one
+            else (if (not carry) AND (xor dig1 dig2)
+                -> one
+                else zero
+|#
+(def get-place-val carry dig1 dig2 = 
+    (_let dig1-bool = (isOne dig1)
+    (_let dig2-bool = (isOne dig2)
+    (_let carry-bool = (isOne carry)
+    (_if ((_and carry-bool) (_not ((xor dig1-bool) dig2-bool)))
+        _then one
+        _else (_if ((_and (_not carry-bool)) ((xor dig1-bool) dig2-bool))
+                _then one
+                _else zero))))))
+
+#|
+    ~ gets next carry value ~
+        if ((carry) AND (or dig1 dig2))
+            -> one
+            else (if (not carry) AND (and dig1 dig2))
+                -> one
+                else zero
+|#
+(def get-new-carry carry dig1 dig2 = 
+    (_let dig1-bool = (isOne dig1)
+    (_let dig2-bool = (isOne dig2)
+    (_let carry-bool = (isOne carry)
+    (_if ((_and carry-bool) ((_or dig1-bool) dig2-bool))
+        _then one
+        _else (_if ((_and (_not carry-bool)) ((_and dig1-bool) dig2-bool))
+                _then one
+                _else zero))))))
+
+
+(def bin-add-helper f l1 l2 carry =
+    (_if (isNil l1)
+      _then l2
+      _else 
+        (_if (isNil l2)
+          _then l1
+          _else
+            (_let place-val = (((get-place-val carry) (head l1)) (head l2))
+                (_let new-carry = (((get-new-carry carry) (head l1)) (head l2))
+                    ((pair place-val) (((f (tail l1)) (tail l2)) new-carry))
+                )
+            )
+        )
+    )
+)
+
+#|
+    1. Reverse their lists since we add right to left and carry to the left
+    2. Pass to bin-add-helper with Y to do work on each digit and then reverse result
+|#
+(def bin-add l1 l2 = (rev ((((Y bin-add-helper) (rev l1)) (rev l2)) zero)))
 
 ;===================================================
 
-#|
-    ~ ADD ~
-    - Contract: (b-dig,b-dig) => b-dig
-    - Idea: m,n => m+n
-|#
+(displayln "truth table for test get-place-val")
+(displayln (n-read (((get-place-val zero) zero) zero)))
+(displayln (n-read (((get-place-val zero) zero) one)))
+(displayln (n-read (((get-place-val zero) one) zero)))
+(displayln (n-read (((get-place-val zero) one) one)))
+(displayln (n-read (((get-place-val one) zero) zero)))
+(displayln (n-read (((get-place-val one) zero) one)))
+(displayln (n-read (((get-place-val one) one) zero)))
+(displayln (n-read (((get-place-val one) one) one)))
+
+(displayln "truth table for test get-new-carry")
+(displayln (n-read (((get-new-carry zero) zero) zero)))
+(displayln (n-read (((get-new-carry zero) zero) one)))
+(displayln (n-read (((get-new-carry zero) one) zero)))
+(displayln (n-read (((get-new-carry zero) one) one)))
+(displayln (n-read (((get-new-carry one) zero) zero)))
+(displayln (n-read (((get-new-carry one) zero) one)))
+(displayln (n-read (((get-new-carry one) one) zero)))
+(displayln (n-read (((get-new-carry one) one) one)))
+
+(displayln "------------------------------")
+
+(def sum-1-2 = ((bin-add bin-one) bin-two))
+
+(displayln "TESTING BIN ADD:")
+(displayln "bin-read sum-1-2")
+(displayln (bin-read sum-1-2))
+(displayln ((l-read sum-1-2) n-read))
+(displayln "len sum-1-2")
+(displayln (n-read (len sum-1-2)))
+(displayln "head of sum-1-2")
+(displayln (n-read (head sum-1-2)))
+(displayln "head of tail of sum-1-2")
+(displayln (n-read (head (tail sum-1-2))))
+
+
+
+
+
+
+
+
+
 
 
 
