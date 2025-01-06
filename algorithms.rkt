0074caaa3e638f5a48167a0a190bcb6c68bc580a
Kyle Serrecchia <kserrec@gmail.com>
2025-01-05 22:28:02 -0800

    implemented bubble sort (#43)

--- algorithms.rkt
+++ algorithms.rkt
@@ -1,7 +1,8 @@
 #lang s-exp "macros/lazy-with-macros.rkt"
 (require "macros/macros.rkt")
 (provide (all-defined-out))
-(require "church.rkt"
+(require "core.rkt"
+         "church.rkt"
          "division.rkt"
          "integers.rkt"
          "lists.rkt"
@@ -16,14 +17,14 @@
     ~ BINARY SEARCH ~
 
     -Algorithm:
-    ; binarySearch (list) (target) (low) (high) = 
+    ; binarySearch (lst) (target) (low) (high) = 
     ;   let mid = ((low+high)/2)
         ; if (low <= high)
-            ; then if (target == list[mid])
+            ; then if (target == lst[mid])
                     ; then mid
-                    ; else if (target < list[mid])
-                        ; then binarySearch (list) (target) (low) (mid-1)
-                        ; else binarySearch (list) (target) (mid+1) (high)
+                    ; else if (target < lst[mid])
+                        ; then binarySearch (lst) (target) (low) (mid-1)
+                        ; else binarySearch (lst) (target) (mid+1) (high)
             ; else badVal
 
    ===================
@@ -33,17 +34,17 @@
         - note1: list must be sorted
         - note2: returns true if element is not in list
 |#
-(def binarySearch list target =
-    (((((Y binarySearch-helper) list) target) zero) (pred (len list))))
+(def binarySearch lst target =
+    (((((Y binarySearch-helper) lst) target) zero) (pred (len lst))))
 
-(def binarySearch-helper f list target low high = 
+(def binarySearch-helper f lst target low high = 
     (_let mid = ((div ((add low) high)) two)
         (_if ((lte low) high)
-            _then (_if ((eq target) ((ind list) mid))
+            _then (_if ((eq target) ((ind lst) mid))
                     _then mid
-                    _else (_if ((lt target) ((ind list) mid))
-                            _then ((((f list) target) low) (pred mid))
-                            _else ((((f list) target) (succ mid)) high)))
+                    _else (_if ((lt target) ((ind lst) mid))
+                            _then ((((f lst) target) low) (pred mid))
+                            _else ((((f lst) target) (succ mid)) high)))
             _else true)))
 
 #|
@@ -53,15 +54,52 @@
         - note2: returns -1 if element is not in list
 |#
 
-(def binarySearchZ list target =
-    (((((Y binarySearchZ-helper) list) target) zero) (pred (len list))))
+(def binarySearchZ lst target =
+    (((((Y binarySearchZ-helper) lst) target) zero) (pred (len lst))))
 
-(def binarySearchZ-helper f list target low high =
+(def binarySearchZ-helper f lst target low high =
     (_let mid = ((div ((add low) high)) two)
         (_if ((lte low) high)
-            _then (_if ((eqZ target) ((ind list) mid))
+            _then (_if ((eqZ target) ((ind lst) mid))
                     _then ((makeZ true) mid)
-                    _else (_if ((ltZ target) ((ind list) mid))
-                            _then ((((f list) target) low) (pred mid))
-                            _else ((((f list) target) (succ mid)) high)))   
+                    _else (_if ((ltZ target) ((ind lst) mid))
+                            _then ((((f lst) target) low) (pred mid))
+                            _else ((((f lst) target) (succ mid)) high)))   
             _else negOne)))
+
+
+;===================================================
+
+; returns a list with two passed elements swapped where left starts at index i
+(def swap lst left i right = 
+    (_let new-lst = (((replace right) lst) i)
+    (((replace left) new-lst) (succ i))))
+
+; bubble pass helper - swaps left and right if need be and then recurses
+(def one-bubble-pass-helper f lst for-i resting-place = 
+    (_if ((gt (succ for-i)) resting-place)
+        _then lst
+        _else 
+            (_let left = ((ind lst) for-i)
+            (_let right = ((ind lst) (succ for-i))
+            (_if ((gt left) right)
+                _then (_let swapped-list = ((((swap lst) left) for-i) right)
+                    (((f swapped-list) (succ for-i)) resting-place))
+                _else (((f lst) (succ for-i)) resting-place))))))
+
+; bubble passes up to "resting-place"
+(def one-bubble-pass lst resting-place = ((((Y one-bubble-pass-helper) lst) zero) (pred resting-place)))
+
+; bubble passes up to each "resting-place" from final index on down
+(def bubble-sort-helper f lst for-i = 
+    (_let lst-len = (len lst)
+    (_if ((gte for-i) lst-len)
+        _then lst
+        _else (_let new-list = ((one-bubble-pass lst) ((for-i pred) lst-len))
+            ((f new-list) (succ for-i))))))
+
+; bubble sort
+(def bubble-sort lst = (((Y bubble-sort-helper) lst) zero))
+
+
+
